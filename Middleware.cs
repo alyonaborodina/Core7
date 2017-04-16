a76481857ebb389aca148c870363e0d72af1add3
alyonaborodina <ms.borodinaa@mail.ru>
2017-04-16 23:51:47 +0500

    Lab7

---
+++ Middleware.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core7
+{
+    public class Middleware
+    {
+
+        private readonly RequestDelegate _next;
+
+        public Middleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var Middle = context.Request.Query["Middle"];
+            if (string.IsNullOrWhiteSpace(Middle) || Middle != "Middleware")
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync("Middle is invalid");
+            }
+            else
+            {
+                await _next.Invoke(context);
+            }
+        }
+    }
+}
+
